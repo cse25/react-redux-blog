4813de5fd739ca597d55d8f8b6cae266c9a81293
chrisjc25@gmail.com <chrisjc25@gmail.com>
2016-09-16 14:29:00 -0700

    create PostsIndex container

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router';
 
-export default () => {
-  return <div>List of blog posts.</div>;
+class PostsIndex extends Component {
+  // define lifecycle method, componentWillMount
+  // is called automatically when component is about to be rendered to DOM for the first time
+  // not called on subsequent rerenders
+  // place fetchPosts action creator here
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="text-xs-right">
+          <Link to="posts/new" className="btn btn-primary">
+            Add a Post
+          </Link>
+        </div>
+        List of Blog Posts
+      </div>
+    );
+  }
 }
+
+export default connect(null, { fetchPosts })(PostsIndex);
+
+// promote from component to container to give it ability to call action creator (mapDispatchToProps)
+// this.props.fetchPosts
